da733c7c1e837dac74bf636a36736b78953aedd9
Char <143526767+char667v@users.noreply.github.com>
2026-03-11 20:55:17 +0100

    ovelse 1 - mr duck greeting virker

--- dist/events.js
+++ dist/events.js
@@ -1,59 +1,31 @@
+import { getUsers } from "./api-functions";
 //alert("Hej Daniel");
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var box = document.getElementById("box");
+const box = document.getElementById("box");
 box.addEventListener("mouseover", function (event) {
     // function runs when user clicks on box
     console.log(event);
     this.style.backgroundColor = "red";
 });
+fetch("/api/users", {
+    headers: {
+        Authorization: "Bearer " + "your jwt here"
+    }
+});
 box.addEventListener("mouseout", function (event) {
     box.style.backgroundColor = "blue";
 });
 ///////////////////////////////////////////////
-var btn = document.getElementById("btnHover");
-var div = document.getElementById("divText");
-btn.addEventListener("mouseover", function () {
+// Hover btn
+const btn = document.getElementById("btnHover");
+const div = document.getElementById("divText");
+btn.addEventListener("mouseover", () => {
     div.classList.remove("hidden");
 });
-btn.addEventListener("mouseout", function () {
+btn.addEventListener("mouseout", () => {
     div.classList.add("hidden");
 });
 ///////////////////////////////////////////////
+// Eksempel
 // const input: any = document.getElementById("inputTodo");
 // const btnAddTodo: HTMLElement | null = document.getElementById("btnTodo");
 // btnAddTodo.addEventListener("click", () => {
@@ -70,66 +42,95 @@ btn.addEventListener("mouseout", function () {
 // });
 ///////////////////////////////////////////////
 // Exercise add date to your duck project - this works but not not as expected; correct it
-var input = document.getElementById("inputTodo");
-var btnAddTodo = document.getElementById("btnTodo");
+const input = document.getElementById("inputTodo");
+const btnAddTodo = document.getElementById("btnTodo");
 // window.addEventListener("load", () => {
-//   // let name = prompt("What is your name?");
-//   // alert("Hello " + name);
+//   let name = prompt("What is your name?");
+//   alert("Hello " + name);
 // });
 // alert(new Date(2026, 1, 27));
-btnAddTodo.addEventListener("click", function () {
-    var todoValue = input.value; // retrieve user input
+btnAddTodo.addEventListener("click", () => {
+    const todoValue = input.value; // retrieve user input
+    console.log(new Date().toLocaleString());
     // create a new li - element
-    var li = document.createElement("li");
+    const li = document.createElement("li");
+    li.title = new Date().toUTCString();
     li.textContent = todoValue; // set its content to ...
     li.addEventListener("click", function () {
-        // add event listener to the new li element
         this.remove(); // remove the li-element the user clicked
     });
     document.getElementById("todos").prepend(li); //add li to ul
     input.value = "";
 });
+// Keep it DRY - Dont Repeat Yourself
+function addErrorLi(errormessage) {
+    const li = document.createElement("li"); // create li element
+    li.textContent = errormessage; // Sets content
+    document.getElementById("errors").appendChild(li); // adds li to ul
+}
 ///////////////////////////////////////////////
 // Form
-document.getElementById("formUser")?.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  var firstname = document.getElementById("firstname").value.trim();
-  var lastname = document.getElementById("lastname").value.trim();
-  var email = document.getElementById("email").value.trim();
-  var password = document.getElementById("password").value.trim();
-
-  var errors = document.getElementById("errors");
-  errors.innerHTML = "";
-
-  if (firstname === "") {
-    var li = document.createElement("li");
-    li.textContent = "Firstname must be filled out";
-    errors.appendChild(li);
-  }
-
-  if (lastname === "") {
-    var li = document.createElement("li");
-    li.textContent = "Lastname must be filled out";
-    errors.appendChild(li);
-  }
-
-  if (email === "") {
-    var li = document.createElement("li");
-    li.textContent = "Email must be filled out";
-    errors.appendChild(li);
-  }
-
-  if (password === "") {
-    var li = document.createElement("li");
-    li.textContent = "Password must be filled out";
-    errors.appendChild(li);
-  }
-
-  if (errors.children.length > 0) return;
-
-  console.log(firstname, lastname, email, password);
+document.getElementById("formUser")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const firstname = document.getElementById("firstname").value.trim();
+    const lastname = document.getElementById("lastname").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errors = document.getElementById("errors");
+    errors.innerHTML = "";
+    if (firstname === "") {
+        const li = document.createElement("li");
+        li.textContent = "Firstname must be filled out";
+        errors.appendChild(li);
+    }
+    if (lastname === "") {
+        const li = document.createElement("li");
+        li.textContent = "Lastname must be filled out";
+        errors.appendChild(li);
+    }
+    if (email === "") {
+        const li = document.createElement("li");
+        li.textContent = "Email must be filled out";
+        errors.appendChild(li);
+    }
+    if (password === "") {
+        const li = document.createElement("li");
+        li.textContent = "Password must be filled out";
+        errors.appendChild(li);
+    }
+    if (errors.children.length > 0)
+        return;
+    console.log(firstname, lastname, email, password);
 });
+// Form Christians version
+// document.getElementById("formUser")
+// .addEventListener("click", (alexanderErISverige: Event) => {
+//     alexanderErISverige.preventDefault(); // stops default form behaviour, to send a post request to server
+//     const firstname = (document.getElementById("firstname") as HTMLInputElement).value;
+//     const lastname = (document.getElementById("lastname") as HTMLInputElement).value;
+//     const email = (document.getElementById("email") as HTMLInputElement).value;
+//     const password = (document.getElementById("password") as HTMLInputElement).value;
+//     console.log(firstname, lastname, email, password);
+//     //const errors = document.getElementById("errors");
+//     document.getElementById("errors").textContent = "";
+//     if (firstname.trim() === "") {
+//         addErrorLi("Firstname must be filled out");
+//     }
+//     if (lastname.trim() === "") {
+//         addErrorLi("Lastname must be filled out");
+//     }
+//     if (email.trim() === "") {
+//         addErrorLi("Email must be filled out");
+//     } else {
+//         if (!email.includes("@")) {
+//             addErrorLi("Email must include a @")
+//         }
+//     }
+//     if (password.trim() === "") {
+//         addErrorLi("Password must be filled out");
+//     }
+// })
+// Form med advanced email udgave
 // document.getElementById("formUser").addEventListener("submit", (e: Event) => {
 //   e.preventDefault();
 //   const firstname = (document.getElementById("firstname") as HTMLInputElement).value;
@@ -194,53 +195,57 @@ document.getElementById("formUser")?.addEventListener("submit", function (e) {
 //   }
 //   console.log(firstname, lastname, email, password);
 // });
+///////////////////////////////////////////////
 // Joke exercise
-// document.getElementById("btnJoke").addEventListener("click", function () {
-//     return __awaiter(this, void 0, void 0, function () {
-//         var url, alexanderSovOverSig, data;
-//         return __generator(this, function (_a) {
-//             switch (_a.label) {
-//                 case 0:
-//                     url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist";
-//                     return [4 /*yield*/, fetch(url, {
-//                             method: 'GET'
-//                         })];
-//                 case 1:
-//                     alexanderSovOverSig = _a.sent();
-//                     if (!alexanderSovOverSigOgDetErOk.ok) {
-//                         alert("Noget gik galt");
-//                     }
-//                     return [4 /*yield*/, alexanderSovOverSigOgDetErOk.json()];
-//                 case 2:
-//                     data = _a.sent();
-//                     console.log(data);
-//                     return [2 /*return*/];
-//             }
-//         });
-//     });
-// });
-
-//Joke
-document.getElementById("btnJoke").addEventListener("click", async function () {
-  const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
-  const x = await fetch(url, { method: "GET" });
-
-  // Svarer serveren med en statuskode som er en fejl
-  if (!x.ok) {
-    alert("Noget gik galt");
-    return;
-  }
-
-  // Konvertér fra json til javascript repræsentation
-  const data = await x.json();
-  console.log(data);
-
-  // ADDITION: vis i browseren
-  const jokeText = document.getElementById("jokeText");
-  if (data.type === "single") {
-    jokeText.textContent = data.joke;
-  } else {
-    jokeText.textContent = data.setup + " " + data.delivery;
-  }
+document.getElementById("btnJoke")?.addEventListener("click", async () => {
+    const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+    const x = await fetch(url, { method: "GET" });
+    // Svarer serveren med en statuskode som er en fejl
+    if (!x.ok) {
+        alert("Noget gik galt");
+        return;
+    }
+    // Konvertér fra json til javascript repræsentation
+    const data = await x.json();
+    console.log(data);
+    // ADDITION: vis i browseren
+    const jokeText = document.getElementById("jokeText");
+    if (!jokeText)
+        return;
+    if (data.type === "single") {
+        jokeText.textContent = data.joke;
+    }
+    else {
+        jokeText.textContent = data.setup + " " + data.delivery;
+    }
+});
+///////////////////////////////////////////////
+//
+async function signup(myUsername, myPassword) {
+    const port = 8080;
+    const backendUrl = "http://localhost:" + port;
+    const response = await fetch(backendUrl, {
+        method: 'POST',
+        body: JSON.stringify({ username: myUsername, password: myPassword })
+    });
+    if (!response.ok) {
+        alert("Something went wrong signing up");
+    }
+    const data = await response.json(); // reads reponse from server, converts from json
+    console.log(data);
+}
+// waiting on page to load, and then run some js
+window.addEventListener("load", async () => {
+    const users = await getUsers();
+    users.map((user) => {
+        const template = document.getElementById("template");
+        const kopi = template.content.cloneNode(true);
+        kopi.getElementById("name").textContent = user.firstname + " " + user.lastname;
+        kopi.getElementById("email").textContent = user.email + " " + user.email;
+        kopi.getElementById("password").textContent = user.password;
+        document.getElementById("userContainer").appendChild(kopi);
+    });
+    // let name = prompt("What is your name?");
+    // alert("Hello, " + name);
 });
 //# sourceMappingURL=events.js.map
